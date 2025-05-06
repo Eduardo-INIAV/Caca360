441d4d8d89f1c33dfffaa0fb6f84002e01fab115
Eduardo-INIAV <eduardo.alves@iniav.pt>
2025-05-06 10:55:43 +0100

    error

--- Pages/LoginPage/LoginPage.xaml.cs
+++ Pages/LoginPage/LoginPage.xaml.cs
@@ -7,10 +7,9 @@ public partial class LoginPage : ContentPage
         InitializeComponent();
         BindingContext = vm;
     }
+    private async void OnRegisterButtonClicked(object sender, EventArgs e)
+        {
+            var registerPage = MauiProgram.ServiceProvider.GetRequiredService<RegisterPage>();
+            await Navigation.PushAsync(registerPage);
+        }
 }
-private async void OnRegisterButtonClicked(object sender, EventArgs e)
-    {
-        var registerPage = MauiProgram.ServiceProvider.GetRequiredService<RegisterPage>();
-        await Navigation.PushAsync(registerPage);
-    }
-
