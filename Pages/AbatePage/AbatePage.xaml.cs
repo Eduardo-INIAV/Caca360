34ab678706c4488cbdd43c6a3307e928b59ccbd1
Eduardo-INIAV <eduardo.alves@iniav.pt>
2025-05-08 16:05:06 +0100

    imagens

--- Pages/AbatePage/AbatePage.xaml.cs
+++ Pages/AbatePage/AbatePage.xaml.cs
@@ -6,7 +6,7 @@ public partial class AbatePage : ContentPage
     {
         InitializeComponent();
 
-        var abateSurveyUrl = "https://survey123.arcgis.com/share/EXEMPLO_ID_FORMULARIO_ABATE";
+        var abateSurveyUrl = "https://portalgeo.iniav.pt/portal/sharing/rest/oauth2/authorize?client_id=survey123hub&response_type=token&expiration=20160&redirect_uri=https%3A%2F%2Fsurvey123.arcgis.com%2Fshare%2F70158155a7a54469a872cfdf63933f19%3FportalUrl%3Dhttps%3A%2F%2Fportalgeo.iniav.pt%2Fportal&resourceItemId=70158155a7a54469a872cfdf63933f19&canHandleCrossOrgSignIn=true";
         AbateSurveyWebView.Source = abateSurveyUrl;
     }
 }
